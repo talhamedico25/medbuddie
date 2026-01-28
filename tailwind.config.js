4fc1da8c0d265e85082e659ac0cabb2a2d2407fc
talhamedico25 <talhakhattak2006@gmail.com>
2026-01-28 20:52:22 +0500

    Add Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./constants.tsx",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-inter)"],
+        serif: ["var(--font-playfair)"],
+      },
+    },
+  },
+  plugins: [],
+};
